d94c671ac5d1c0051f5635b897da1f7112471ca9
usamasarfraz098 <121352973+usamasarfraz098@users.noreply.github.com>
2024-02-17 01:39:18 +0500

    d

--- script.js
+++ script.js
@@ -1,57 +1,55 @@
 'use strict';
-// const value = document.querySelector('.number').innerHTML;
-// const guessNumber = document.querySelector('.number').textContent;
-const message = document.querySelector('.message');
-const score = document.querySelector('.score').innerHTML;
-const highscore = document.querySelector('.highscore').innerHTML;
-const checkButton = document.querySelector('.check');
-let score2 = 20;
-const randomValue = Math.trunc(Math.random() * 20 + 1);
-const againBtn = document.querySelector('.again');
-againBtn.addEventListener('click', function () {
-  message.innerHTML = 'Start guessing...';
-  document.querySelector('.score').innerHTML = 20;
-  document.querySelector('.number').innerHTML = '?';
-  document.querySelector('.highscore').innerHTML = 0;
-  document.querySelector('body').style.backgroundColor = '#222';
-});
 
-const score1 = randomValue;
-console.log(score1);
+const message = document.querySelector('.message');
+const scoreDisplay = document.querySelector('.score');
+const highscoreDisplay = document.querySelector('.highscore');
+const checkButton = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+
+let score = 20;
+let highscore = 0;
+let randomValue;
+
+function generateRandomNumber() {
+  randomValue = Math.trunc(Math.random() * 20 + 1);
+}
+generateRandomNumber();
+
+
+againBtn.addEventListener('click', function () {
+  message.textContent = 'Start guessing...';
+  score = 20;
+  scoreDisplay.textContent = score;
+  document.querySelector('.number').textContent = '?';
+  highscore = 0;
+  highscoreDisplay.textContent = highscore;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.guess').value = '';
+  generateRandomNumber(); 
+});
 
 checkButton.addEventListener('click', function () {
   const inputNumber = parseInt(document.querySelector('.guess').value);
-  if (inputNumber !== score1) {
-    message.innerHTML =
-      inputNumber > score1 ? 'number is greater' : 'number is smaller';
-    if (score2 > 1) {
-      score2 = score2 - 1;
-      document.querySelector('.score').innerHTML = score2;
+
+  if (!inputNumber) {
+    message.textContent = 'No number entered!';
+  } else if (inputNumber !== randomValue) {
+    message.textContent = inputNumber > randomValue ? 'Number is greater' : 'Number is smaller';
+    if (score > 1) {
+      score--;
+      scoreDisplay.textContent = score;
     } else {
-      message.innerHTML = 'you lose the game';
+      message.textContent = 'You lose the game';
     }
-  }
-  //   else if (inputNumber < score1) {
-  //     if (score2 > 1) {
-  //       score2 = score2 - 1;
-  //       document.querySelector('.score').innerHTML = score2;
-  //       message.innerHTML = 'number is smaller';
-  //     } else {
-  //       message.innerHTML = 'you lose the game';
-  //     }
-  //   }
-  else {
+  } else {
     document.querySelector('.number').textContent = randomValue;
     document.querySelector('body').style.backgroundColor = 'green';
+    message.textContent = 'Great, you win!';
     if (score > highscore) {
-      document.querySelector('.highscore').innerHTML = score;
-      message.innerHTML = 'great you win';
+      highscore = score;
+      highscoreDisplay.textContent = highscore;
     }
   }
 });
 
-const btnNew= document.querySelector('.newbtn')
-const number=document.querySelector('.number')
-btnNew.addEventListener('click',()=> number.innerHTML='hello')
-console.log(highscore);
-console.log(typeof inputNumber);
+console.log(randomValue);
